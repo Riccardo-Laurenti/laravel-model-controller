203cda1096be766321fb43c7f0935242b4575fb0
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-08-25 15:13:34 +0200

    first commit

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <h6 class="card-title">{{ $movie->original_title }}</h6>
+                            <h6 class="card-title">{{ $movie->nationality }}</h6>
+                            <p class="card-text">{{ $movie->date }}</p>
+                            <p class="card-text">{{ $movie->vote }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</body>
+
+</html>
